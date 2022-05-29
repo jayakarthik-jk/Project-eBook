7d301550e7ab0e7262ca65f085f12f4a91c25349
JayaKarthik.jk <98959858+jayakarthik-jk@users.noreply.github.com>
2022-05-29 11:24:23 +0530

    Changed Search Container Layout
    And also added book.html file

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -13,50 +13,38 @@ window.addEventListener("load", (event) => {
 });
 
 function setBookComponent(params) {
-  container.innerHTML = container.innerHTML + params;
-  document.querySelectorAll(".card-book").forEach((card) => {
-    let img = card.children[0].children[0];
-    let details = card.children[1];
-    details.style.display = "none";
-    img.addEventListener("click", () => {
-      if (details.style.display == "none") {
-        card.style.display = "flex";
-        details.style.display = "block";
-      } else {
-        card.style.display = "inline-flex";
-        details.style.display = "none";
-      }
-    });
-  });
+  container.innerHTML += params;
 }
 
 function getBookComponent(params) {
   params.title = params.title.trim();
   return `
-    <div class="card-book" id="card-book">
-    <div class="col-2">
-      <img
-        src="${params.thumbnail}"
-        alt="Can't load book image"
-        width="150"
-        height="200"
-        id="book-img"
-        loading="lazy"
-      />
-    </div>
-    <div class="col-10 card-details" id="card-details">
-      <p><b>${params.title}</b></p>
-      <p><b>${params.author}</b></p>
-      <p>Description${params.description}</p>
-      <p>Total Pages - ${params.pageCount}</p>
-    </div>
-  </div>
+  <div class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class=" col-6 col-sm-4 col-md-3 col-lg-2 p-2">
+          <img
+            src="${params.thumbnail}"
+            alt="Can't load book image"
+            width="150"
+            height="200"
+            id="book-img"
+            class="book-img"
+            loading="lazy"
+          />
+        </div>
+        <div class="col-6 col-sm-8 col-md-9 col-lg-10 d-flex flex-column">
+          <h5 class="p-2">${params.title}</h5>
+          <h6 class="p-2">Author : ${params.author}</h6>
+          <p class="p-2">Pages : ${params.pageCount}</p>
+          <br />
+          <a href="#" class=" p-2 m-2 mt-auto align-self-end"><button class="btn btn-outline-danger">View Book</button></a>
+        </div>
+      </div>
+      </div>
   `;
 }
-
 async function getBooks(params) {
   try {
-    container.innerHTML = "";
     const response = await fetch(
       `https://www.googleapis.com/books/v1/volumes?q=intitle:${params}&country=IN&maxResults=40`
     );
@@ -65,7 +53,6 @@ async function getBooks(params) {
       try {
         let bookData = {
           title: undefined,
-          subtitle: undefined,
           author: "",
           description: undefined,
           pageCount: undefined,
@@ -85,11 +72,6 @@ async function getBooks(params) {
                 } else {
                   return;
                 }
-                if ("subtitle" in book.volumeInfo) {
-                  bookData.subtitle = book.volumeInfo.subtitle;
-                } else {
-                  return;
-                }
                 if ("description" in book.volumeInfo) {
                   bookData.description = book.volumeInfo.description;
                 } else {
@@ -106,8 +88,9 @@ async function getBooks(params) {
                   return;
                 }
                 if ("authors" in book.volumeInfo) {
+                  bookData.author = "";
                   book.volumeInfo.authors.forEach((author) => {
-                    bookData.author += "\t" + author;
+                    bookData.author += "  " + author;
                   });
                 } else {
                   return;
